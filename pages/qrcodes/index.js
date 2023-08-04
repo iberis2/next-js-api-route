cb738c8e23c1272c9ce6fd81d6ca400deb3a1ccd
iberis2 <snow9402@gmail.com>
2023-08-04 19:02:54 +0900

    Feat: 주소 단축화 페이지

---
+++ pages/qrcodes/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import QRCodeList from '@/components/QRCodeList'
+import Button from '@/components/Button'
+import Link from '@/components/Link'
+import styles from '@/styles/QRCodeListPage.module.css'
+import dbConnect from '@/db/dbConnect'
+import QRCode from '@/db/models/QRCode'
+import axios from '@/lib/axios'
+import { useState } from 'react'
+
+export const getServerSideProps = async () => {
+  await dbConnect()
+  const qrCodes = await QRCode.find()
+
+  return { props: { qrCodes: JSON.parse(JSON.stringify(qrCodes)) } }
+}
+
+export default function QRCodeListPage({ qrCodes: initialValues }) {
+  const [qrCodes, setQrCodes] = useState(initialValues)
+
+  const handleDelete = async id => {
+    await axios.delete(`/qrcodes/${id}`)
+    setQrCodes(prev => prev.filter(qrcode => qrcode._id !== id))
+  }
+
+  return (
+    <>
+      <Head>
+        <title>QRCode 만들기 - Shortit</title>
+      </Head>
+      <div className={styles.page}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>QRCode 만들기</h1>
+          <Button as={Link} href='/qrcodes/new'>
+            새로 만들기
+          </Button>
+        </header>
+        <QRCodeList items={qrCodes} onDelete={handleDelete} />
+      </div>
+    </>
+  )
+}
